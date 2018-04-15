308a2cf82695c20725d8bae504a53ae140b198d7
Priyansh <priyanshzalavadiya007@gmail.com>
2018-04-15 16:22:30 +0530

    myfdupdated

--- php/myfd.php
+++ php/myfd.php
@@ -4,7 +4,24 @@
 	<title>myfd</title>
 </head>
 <body>
-	
+	<div>
+		<?php
+		session_start();
+		$usid=$_SESSION["user"];
+		$data = mysqli_connect("localhost","root","","bank") or die();
+			$db=mysqli_query($data,"SELECT `fid`,`opendate`,`closedate`,`interest`,`startmoney`,`endmoney` FROM fd WHERE `uid`='$usid'");
+
+			foreach ($db as $dc) {
+				$od=$dc['opendate'];
+				$cd=$dc['closedate'];
+				$it=$dc['interest'];
+				$sm=$dc['startmoney'];
+				$em=$dc['endmoney'];
+				echo "$sm is fixed on $od last date is $cd final amount will be $em with interest rate $it<br>";
+				
+			}
+		?>
+	</div>
 
 </body>
 </html>
